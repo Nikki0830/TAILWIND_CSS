6f4445918c2bc7a7e52ef822c92e5f772620486a
Nikki0830 <gniki0830@gmail.com>
2024-02-01 23:13:46 +0530

    TAILWIND_CSS_UDEMY

---
+++ TailwindCLI/tailwind.config1.js
@@ -0,0 +1,11 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  //public folder ** means all folder inside public *.html means all html files
+  content: ["./public/**/*.html"],
+  darkMode:"class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
